9af4a0bd2df45056ea2954308e57fa72396302cc
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-07-12 04:02:18 +0900

    config and initializer

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'FLX_DB_NAME');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'FLX_DB_USER');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'FLX_DB_PASS');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'FLX_DB_HOST');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
